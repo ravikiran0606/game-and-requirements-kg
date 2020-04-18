9174657557c6779afba60bbef3d3f9cf4050f8ed
Ravi Kiran Selvam <sravikiran0606@gmail.com>
2020-04-18 13:33:11 -0700

    Update ER for G2A Games GPUs vs Techpowerup GPUs with blocking

--- 2_entity_resolution/ER_g2a_games_gpus_and_techpowerup_gpus.py
+++ 2_entity_resolution/ER_g2a_games_gpus_and_techpowerup_gpus.py
@@ -2,9 +2,20 @@ import numpy as np
 import json
 import jsonlines
 import rltk
+import re
+import time
 
-techpowerup_gpus = None
 g2a_games = None
+gpu_blocks = None
+techpowerup_gpus = None
+gpu_vocab = {}
+
+def containsDigit(input_str):
+    for i in input_str:
+        if i.isdigit():
+            return True
+
+    return False
 
 def constructDictfromJL(json_lines_file):
     result_dict = {}
@@ -17,10 +28,71 @@ def constructDictfromJL(json_lines_file):
 
     return result_dict
 
-def getMostSimilarGPU_Techpowerup(input_gpu):
-    global techpowerup_gpus
+def createBlocks():
+    global gpu_vocab, techpowerup_gpus
 
-    split_words = ["/", "or"]
+    cur_blocks = {}
+    block_keys = ["radeon", "geforce", "quadro", "others"]
+    for cur_key in block_keys:
+        cur_blocks[cur_key] = dict()
+
+    for key, val in techpowerup_gpus.items():
+        try:
+            cur_product_name = val["Product Name"].lower()
+            split_list = cur_product_name.split()
+            for cur_word in split_list:
+                if not containsDigit(cur_word):
+                    gpu_vocab[cur_word] = 1
+
+            flag = 0
+            for cur_key in block_keys:
+                if cur_key in cur_product_name:
+                    cur_blocks[cur_key][key] = 1
+                    flag = 1
+                    break
+
+            if flag == 0:
+                cur_blocks["others"][key] = 1
+        except:
+            pass
+
+    return cur_blocks
+
+def cleanGPUText(input_gpu):
+    global gpu_vocab
+
+    # Removing sizes (mb, gb)
+    input_gpu = re.sub("\d+(mb|gb)", "", input_gpu)
+    input_gpu = re.sub("^\d+(mb|gb)", "", input_gpu)
+    input_gpu = re.sub("^\d+", "", input_gpu)
+    input_gpu = re.sub("(mb|gb)", "", input_gpu)
+    input_gpu = input_gpu.replace("®", "")
+
+    cur_split = input_gpu.split()
+    result_gpu = ""
+    for cur_word in cur_split:
+        if containsDigit(cur_word):
+            result_gpu += " " + cur_word
+        elif gpu_vocab.get(cur_word) is not None:
+            result_gpu += " " + cur_word
+
+    result_gpu = result_gpu.strip()
+    return result_gpu
+
+def getGPUBlockKey(input_gpu):
+    global gpu_blocks
+    for cur_key in gpu_blocks.keys():
+        if cur_key in input_gpu:
+            return cur_key
+
+    return "others"
+
+
+# Current Problems: (8670 vs 4670) and (6600 vs 600)
+def getMostSimilarGPU_Techpowerup(input_gpu):
+    global gpu_blocks, techpowerup_gpus
+
+    split_words = [" / ", ", ", " or "]
 
     game_gpus = [input_gpu]
     for cur_word in split_words:
@@ -28,51 +100,109 @@ def getMostSimilarGPU_Techpowerup(input_gpu):
             game_gpus = input_gpu.split(cur_word)
             break
 
+
+    gpu1 = {}
+    gpu2 = {}
+
+    # Base Cases:
+    # Invalid input gpu
+    if game_gpus[0] == "-" and len(game_gpus) == 1:
+        return gpu1, gpu2
+
+    # GPU1
     max_score = -1
     max_match_id = None
-    for tgpu_id, tgpu_val in techpowerup_gpus.items():
+    max_match_val = None
+    cur_game_gpu = cleanGPUText(game_gpus[0].lower())
+    gpu1["actual_val"] = game_gpus[0].lower()
+    cur_block_key = getGPUBlockKey(cur_game_gpu)
+    for tgpu_id in gpu_blocks[cur_block_key].keys():
         try:
+            tgpu_val = techpowerup_gpus[tgpu_id]
             cur_product_name = tgpu_val["Product Name"].lower()
-            cur_game_gpu = game_gpus[0].lower()
+
             cur_score = rltk.levenshtein_similarity(cur_game_gpu, cur_product_name)
+
             if cur_score > max_score:
                 max_score = cur_score
                 max_match_id = tgpu_id
+                max_match_val = cur_product_name
         except:
             pass
 
-    return max_match_id, max_score
+    gpu1["max_score"] = max_score
+    gpu1["max_match_id"] = max_match_id
+    gpu1["max_match_val"] = max_match_val
+
+    # GPU2
+    if len(game_gpus) >= 2:
+        max_score = -1
+        max_match_id = None
+        max_match_val = None
+        cur_game_gpu = cleanGPUText(game_gpus[1].lower())
+        gpu2["actual_val"] = game_gpus[1].lower()
+        cur_block_key = getGPUBlockKey(cur_game_gpu)
+        for tgpu_id in gpu_blocks[cur_block_key].keys():
+            try:
+                tgpu_val = techpowerup_gpus[tgpu_id]
+                cur_product_name = tgpu_val["Product Name"].lower()
+
+                cur_score = rltk.levenshtein_similarity(cur_game_gpu, cur_product_name)
+
+                if cur_score > max_score:
+                    max_score = cur_score
+                    max_match_id = tgpu_id
+                    max_match_val = cur_product_name
+            except:
+                pass
+
+        gpu2["max_score"] = max_score
+        gpu2["max_match_id"] = max_match_id
+        gpu2["max_match_val"] = max_match_val
+
+    return gpu1, gpu2
 
 if __name__ == "__main__":
     g2a_games_file = "../../data_with_ids/sample_g2a_games_with_requirements.jl"
     techpowerup_gpu_file = "../../data_with_ids/techpowerup_gpu_specs.jl"
-    out_file = "ER_g2a_games_gpus_and_techpowerup_gpus.jl"
+    out_file = "ER_g2a_games_gpus_and_techpowerup_gpus_short_2.jl"
 
     techpowerup_gpus = constructDictfromJL(techpowerup_gpu_file)
+    gpu_blocks = createBlocks()
     g2a_games = constructDictfromJL(g2a_games_file)
     er_mapping_result = []
+    start_time = time.time()
 
     index = 0
+    match_cnt = 0
+    nonmatch_cnt = 0
     for key, val in g2a_games.items():
         if index%100==0:
-            print("Progress count = ", index)
+            cur_time = time.time()
+            seconds_elapsed = cur_time - start_time
+            print("Progress count = ", index, ", Seconds elapsed = ", seconds_elapsed)
 
         cur_dict = {}
         cur_dict["g2a_games_id"] = key
         try:
             min_req = val["min_requirements"]
             cur_gpu = min_req["Graphics"]
-            tpowerup_id, score = getMostSimilarGPU_Techpowerup(cur_gpu)
-            # if score >= 0.5:
-            #     print(cur_gpu, "-------", techpowerup_gpus[tpowerup_id]["Product Name"])
-            cur_dict["techpowerup_gpu_id"] = tpowerup_id
-            cur_dict["similarity_score"] = score
+            tpowerup_gpu1, tpowerup_gpu2 = getMostSimilarGPU_Techpowerup(cur_gpu)
+            cur_dict["tpowerup_gpu1"] = tpowerup_gpu1
+            cur_dict["tpowerup_gpu2"] = tpowerup_gpu2
+            match_cnt += 1
         except:
-            cur_dict["techpowerup_gpu_id"] = "NA"
-            cur_dict["similarity_score"] = -1
+            cur_dict["tpowerup_gpu1"] = {}
+            cur_dict["tpowerup_gpu2"] = {}
+            nonmatch_cnt += 1
 
         er_mapping_result.append(cur_dict)
         index = index + 1
+
+    cur_time = time.time()
+    seconds_elapsed = cur_time - start_time
+    print("Progress count = ", index, ", Seconds elapsed = ", seconds_elapsed)
+    print("Matches = ", match_cnt, ", Non Matches = ", nonmatch_cnt)
 
     with jsonlines.open(out_file, "w") as f:
         f.write_all(er_mapping_result)
