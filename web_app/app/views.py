59708c7930bf9abb08c44af4ae8e683c8c151f44
Ravi Kiran Selvam <sravikiran0606@gmail.com>
2020-04-24 22:46:18 -0700

    Update views to include new routes

--- web_app/app/views.py
+++ web_app/app/views.py
@@ -5,3 +5,15 @@ from flask import Flask, flash, render_template, json, request, redirect, sessio
 @app.route('/')
 def main():
     return render_template('index.html')
+
+@app.route('/query')
+def queryPage():
+    return render_template('query.html')
+
+@app.route('/game')
+def gamePage():
+    return render_template('game.html')
+
+@app.route('/visualize')
+def visualizationPage():
+    return render_template('visualization.html')
